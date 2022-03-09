f6dc630ab7993006d3d5efd8433c8c4057ee021f
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-03-09 02:01:22 -0500

    Por revisar errores antes de proceder al testing

--- src/test/java/co/com/deliversk/usecases/CrearOrdenUseCaseTest.java
+++ src/test/java/co/com/deliversk/usecases/CrearOrdenUseCaseTest.java
@@ -1,7 +1,7 @@
 package co.com.deliversk.usecases;
 
-import co.com.deliversk.domain.transporte.OrdenCreada;
 import co.com.deliversk.domain.transporte.command.CrearOrden;
+import co.com.deliversk.domain.transporte.event.OrdenCreada;
 import co.com.deliversk.domain.transporte.event.TransporteCreado;
 import co.com.deliversk.domain.transporte.valor.*;
 import co.com.sofka.business.generic.UseCaseHandler;

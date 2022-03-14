d413b5d44edde10fe86cfdaf503fe5b850afa1a9
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-03-14 17:48:42 -0500

    Actualización del UseCase de actualizar destinatario por falta de TransporteId y OrdenId

--- src/main/java/co/com/deliversk/domain/transporte/command/ActualizarDestinatarioOrden.java
+++ src/main/java/co/com/deliversk/domain/transporte/command/ActualizarDestinatarioOrden.java
@@ -1,18 +1,36 @@
 package co.com.deliversk.domain.transporte.command;
 
 import co.com.deliversk.domain.transporte.valor.Destinatario;
+import co.com.deliversk.domain.transporte.valor.OrdenId;
+import co.com.deliversk.domain.transporte.valor.TransporteId;
 import co.com.sofka.domain.generic.Command;
 
 public class ActualizarDestinatarioOrden extends Command {
     private final Destinatario destinatario;
+    private final TransporteId transporteId;
+    private final OrdenId ordenId;
 
-    public ActualizarDestinatarioOrden(Destinatario destinatario) {
+    public ActualizarDestinatarioOrden(TransporteId transporteId, OrdenId ordenId,Destinatario destinatario) {
+        this.transporteId = transporteId;
+        this.ordenId = ordenId;
         this.destinatario = destinatario;
     }
 
     public Destinatario getDestinatario() {
         return destinatario;
     }
+
+    public TransporteId getTransporteId() {
+        return transporteId;
+    }
+
+    public OrdenId getOrdenId() {
+        return ordenId;
+    }
+
+    
+
+    
 
     
 
